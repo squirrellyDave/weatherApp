dbea21335b7d276a927c22cb15b4709de13d9877
David Edmonds <git@edmonds.site>
2020-03-23 16:04:47 -0400

    Updated app.js files for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
-
+const port = process.env.PORT || 3000
 
 // Path Definitions for express config 
 const pathToPublic = path.join(__dirname, '../public')
@@ -94,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on Port 3000')
+app.listen(port, () => {
+    console.log('Server is up on Port ' + port)
 })
